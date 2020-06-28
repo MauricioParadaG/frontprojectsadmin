9d477f5b5dbb62ade8b4796f3693da9d35d475b0
MauricioParadaG <62025899+MauricioParadaG@users.noreply.github.com>
2020-06-28 10:02:05 +0200

    New MERN Project, creating the routes
    New MERN Project, creating the routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import LoginComponent from './components/auth/Login.component';
+import NewAccountComponent from './components/auth/NewAccount.component';
+import ProjectsComponent from './components/projects/Projects.component';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+
+        <Route exact path="/" component={LoginComponent}/>
+        <Route exact path="/new-account" component={NewAccountComponent}/>
+        <Route exact path="/projects" component={ProjectsComponent}/>
+        
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/auth/NewAccount.component.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NewAccountComponent = () => {
+    return (
+        <h1>New Account</h1>
+    )
+}
+
+export default NewAccountComponent;
