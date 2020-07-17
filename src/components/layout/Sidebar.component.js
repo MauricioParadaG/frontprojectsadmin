857a27797668b1f5a3b727fa576393051e096328
MauricioParadaG <62025899+MauricioParadaG@users.noreply.github.com>
2020-07-17 22:29:12 +0200

    Creating the responsive left bar in Bimmo
    Creating the responsive left bar in Bimmo

--- src/components/layout/Sidebar.component.js
+++ src/components/layout/Sidebar.component.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import NewProjectComponent from '../projects/NewProject.component';
 import ListedProjectsComponent from '../projects/ListedProjects.component';
 
